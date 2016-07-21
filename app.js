26111822eb6c27175bc92cadde5ace103da4b6fd
Nahuel Lucero <nahuel.bm6@gmail.com>
2016-07-21 10:23:41 -0300

    Adding bootstrap styles + posts service

--- app.js
+++ app.js
@@ -3,25 +3,43 @@
 
   angular
     .module('flapperNews', [])
-    .controller('MainCtrl', ['$scope', MainCtrl])
+    .controller('MainCtrl', ['$scope', 'postsService', MainCtrl])
+    .factory('postsService', [PostsService])
 
-  function MainCtrl($scope) {
+  function MainCtrl($scope, postsService) {
       var vm = this
 
-      vm.posts = [
-        { title: 'post 1', upvotes: 5  },
-        { title: 'post 2', upvotes: 2  },
-        { title: 'post 3', upvotes: 15 },
-        { title: 'post 4', upvotes: 9  },
-        { title: 'post 5', upvotes: 4  }
-      ]
+      vm.posts = postsService.posts
 
       vm.addPost = function() {
-        vm.posts.push({ title: vm.title, upvotes: 0 })
+        if (!vm.title) return
+
+        vm.posts.push({
+          title: vm.title,
+          link: vm.link,
+          upvotes: 0
+        })
+
+        vm.title = ''
+        vm.link = ''
       }
 
       vm.incrementUpvotes = function(post) {
         post.upvotes++
       }
+  }
+
+  function PostsService() {
+    var posts = [
+      { title: 'post 1', upvotes: 5  },
+      { title: 'post 2', upvotes: 2  },
+      { title: 'post 3', upvotes: 15 },
+      { title: 'post 4', upvotes: 9  },
+      { title: 'post 5', upvotes: 4  }
+    ]
+
+    return {
+      posts: posts
+    }
   }
 }());
